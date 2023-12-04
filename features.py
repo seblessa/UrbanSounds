513af4a5e742e82b0d8135f4525ab9fe630bf477
alexandre <alexandrem2003@gmail.com>
2023-12-04 16:13:05 +0000

    comments

--- features.py
+++ features.py
@@ -5,16 +5,20 @@ import numpy as np
 import pandas as pd
 
 
+# Function to extract features from a given audio file
 def extract_features(audio_file, target_duration=4, target_sr=44100):
+
+    # Load audio and define target sample rate
     y, sr = librosa.load(audio_file, sr=target_sr)
 
-    # Ensure audio is target duration
+    # Ensure audio length is the same as the target duration using zero padding
     target_length = int(target_sr * target_duration)
     y = librosa.util.fix_length(y, size=target_length)
 
     # Normalize amplitude
     y = librosa.util.normalize(y)
 
+    # Define variables for some features
     hop_length = 512
     oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length)
 
@@ -36,6 +40,7 @@ def extract_features(audio_file, target_duration=4, target_sr=44100):
     tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr, hop_length=hop_length)
     fourier_tempogram = librosa.feature.fourier_tempogram(onset_envelope=oenv, sr=sr, hop_length=hop_length)
 
+    # Create a dictionary to store the mean value of the features
     features = {
         'chroma_stft': np.mean(chroma_stft),
         'chroma_cqt': np.mean(chroma_cqt),
@@ -52,11 +57,13 @@ def extract_features(audio_file, target_duration=4, target_sr=44100):
         'fourier_tempogram': np.mean(fourier_tempogram)
     }
 
+    # Add the mean of each MFCC feature to the dictionary
     for i in range(1, 41):
         features[f'mcffs_{i}'] = np.mean(mfccs[i - 1])
     return features
 
 
+# Function to iterate through each audio folder and extract features from each file
 def process_data(base_dir):
     for folder in os.listdir(base_dir):
         label_list = []
@@ -71,8 +78,10 @@ def process_data(base_dir):
                     features_list.append(features)
                     label_list.append(label)
 
+        # Create DataFrame for each folder
         df = pd.DataFrame(features_list)
         df['Label'] = label_list
+        # Save DataFrame as a CSV file
         df.to_csv('urbansounds_features' + folder + '.csv', index=False)
         # Display the DataFrame
         print(df.head())
